d283face15ddfdb17020f56d26e10c4e6fc24b0e
TSystem <2329192@mail.ru>
2023-12-04 11:05:35 +0700

    FIrst

---
+++ Window/AddEditPacient.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace DaripProgrammaUP.Window;
+
+public partial class AddEditPacient : Avalonia.Controls.Window
+{
+    public AddEditPacient()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnSavet_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Window/AddEditProcedureWindow.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace DaripProgrammaUP.Window;
+
+public partial class AddEditProcedureWindow :  Avalonia.Controls.Window
+{
+    public AddEditProcedureWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnSavet_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
